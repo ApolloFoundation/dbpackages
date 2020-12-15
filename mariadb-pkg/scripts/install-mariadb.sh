cdb6de7fa487aded2207e9984fc17a5faff5e82e
azinchenko <andrew.zinchenko@gmail.com>
2020-12-15 15:15:57 +0200

    [NO_TICKET] fixed sed arguments for MacOs

--- mariadb-pkg/scripts/install-mariadb.sh
+++ mariadb-pkg/scripts/install-mariadb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo_error()   { echo $'\E[1m\E[31m' "Error: " $'\E[22m\E[39m'$@ ; }
+exit_error()   { echo_error $@ ; exit 1; }
 print_help(){
 cat <<__ENDHELP__
 Usage: $0 [OPTIONS]
@@ -6,21 +8,29 @@ Apollo Foundation. Install mariadb package.
 
   --apl-basedir path   The path to the ApolloWallet installation directory.
   --apl-db-dir path    The path to the Apollo data base directory.
-  --apl-conf-dir path  The path to the local apl-blockchain.properties file.
-  -u|--user user_name  The login username to use for running mariadbd. System user for default.
-  -p|--port port       The MariaDb daemon port. 3366 for default
+
+  -u|--user user_name  The login username to use for running mariadbd or system user by default.
+  -p|--port port       The MariaDb daemon port. The default port is 3366.
   -h|--help            Show help message.
 
 example:
 
-./install-mariadb.sh --apl-db-dir /home/user/.apl-blockchain/apl-blockchain-db --apl-conf-dir /home/user/.apl-blockchain/conf-tn2
+./install-mariadb.sh --apl-db-dir /home/user/.apl-blockchain/apl-blockchain-db
 
 __ENDHELP__
 }
-echo_error()   { echo $'\E[1m\E[31m' "Error: " $'\E[22m\E[39m'$@ ; }
-exit_error()   { echo_error $@ ; exit 1; }
 
-BASE_DIR=$(dirname $(readlink -e "$0"))
+# "linux-gnu"*  - Linux 
+# "cygwin"      - POSIX compatibility layer and Linux environment emulation for Windows
+# "msys"        - Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+# "win32"
+# "freebsd"*
+if [[ "$OSTYPE" == "darwin"* ]]; then
+# Mac OSX
+  BASE_DIR=$(cd "`dirname "$0"`"; pwd -P)
+else
+  BASE_DIR=$(dirname $(readlink -e "$0"))
+fi
 
 cmd_params=$@
 #if [[ $# -lt 2 ]]; then
@@ -33,7 +43,6 @@ db_port="3366"
 db_user=${USER}
 apl_base_dir_param=""
 apl_db_dir_param=""
-apl_conf_dir_param=""
 
 while [ -n "$1" ]; do
     case $1 in
@@ -44,10 +53,6 @@ while [ -n "$1" ]; do
         --apl-db-dir)
             shift
             apl_db_dir_param=$1
-        ;;
-        --apl-conf-dir)
-            shift
-            apl_conf_dir_param=$1
         ;;
         -p|--port)
             shift
@@ -62,7 +67,7 @@ while [ -n "$1" ]; do
             exit 1
         ;;
         *)
-            print_help  
+            print_help
             exit 1
         ;;
     esac
@@ -83,34 +88,12 @@ else
   apl_db_dir="${apl_base_dir}/apollo-blockchain-db"
 fi
 
-if [[ -n ${apl_conf_dir_param} ]]; then
-  apl_conf_dir=${apl_conf_dir_param}
-else
-  apl_conf_dir="${apl_base_dir}/conf"
-fi
-
 apl_mariadb_pkg_dir="${apl_base_dir}/apollo-mariadb"
 db_tmp_dir="${apl_db_dir}/tmp"
 db_data_dir="${apl_db_dir}/data"
-apl_property_file="${apl_conf_dir}/apl-blockchain.properties"
 apl_mariadb_cnf_template_file="${apl_mariadb_pkg_dir}/conf/my-apl.cnf.template"
 apl_mariadb_cnf_file="${apl_mariadb_pkg_dir}/conf/my-apl.cnf"
 mariadb_install_script="${apl_mariadb_pkg_dir}/scripts/mariadb-install-db"
-
-#Insert or Update property
-#$1 properties file
-#$2 property name
-#$3 property value, delete property if value is empty 
-set_property_value() {
-local properties_file=$1
-local property=$2
-local value=$3
-
-  sed -i "/${property}/ d;" $properties_file
-  if [[ -n $value ]] ; then 
-    sed -i "1 i ${property}=${value}" $properties_file
-  fi
-}
 
 #Get property value
 #$1 properties file
@@ -121,19 +104,9 @@ local property=$2
   cat $properties_file | grep -o "$property\ *=[^ ]*"|sed 's/\"//g'|cut -d= -f2
 }
 
-#Patch db properties
-# apl.databasePort=3366, apl.dbUsername=root, apl.dbPassword=root
-#
-patch_blockchain_properties() {
-
-  set_property_value ${apl_property_file} "apl.databasePort" ${db_port}
-  set_property_value ${apl_property_file} "apl.dbUsername" ${db_user}
-  set_property_value ${apl_property_file} "apl.dbPassword" ${db_user}
-}
-
 check_mariadb_pkg() {
-  [ ! -x ${mariadb_install_script} ] && exit_error "File not found: ${mariadb_install_script}."
-  [ ! -f ${apl_mariadb_cnf_file} ] && exit_error "File not found: ${apl_mariadb_cnf_file}."
+  [ ! -x ${mariadb_install_script} ] && exit_error "Executable script not found: ${mariadb_install_script}."
+  [ ! -f ${apl_mariadb_cnf_template_file} ] && exit_error "Cnf template not found: ${apl_mariadb_cnf_template_file}."
 }
 
 #Patch the MariaDB configuration file
@@ -142,20 +115,19 @@ patch_mariadb_cnf() {
 
   cp ${apl_mariadb_cnf_template_file} ${apl_mariadb_cnf_file}
 
-  sed -r "s/(port)=(.+)/\1=${db_port}/g" -i ${apl_mariadb_cnf_file}
+  sed -i ".bak" "s/port=(.+)/port=${db_port}/g" ${apl_mariadb_cnf_file}
 
   pattern='${apl_db_dir}'
-  sed -i "s|${pattern}|${apl_db_dir}|g" ${apl_mariadb_cnf_file}
-
+  sed -i ".bak" "s|${pattern}|${apl_db_dir}|g" ${apl_mariadb_cnf_file}
 
   pattern='${db_data_dir}'
-  sed -i "s|${pattern}|${db_data_dir}|g" ${apl_mariadb_cnf_file}
+  sed -i ".bak" "s|${pattern}|${db_data_dir}|g" ${apl_mariadb_cnf_file}
 
   pattern='${db_tmp_dir}'
-  sed -i "s|${pattern}|${db_tmp_dir}|g" ${apl_mariadb_cnf_file}
+  sed -i ".bak" "s|${pattern}|${db_tmp_dir}|g" ${apl_mariadb_cnf_file}
 
   pattern='${apl_mariadb_pkg_dir}'
-  sed -i "s|${pattern}|${apl_mariadb_pkg_dir}|g" ${apl_mariadb_cnf_file}
+  sed -i ".bak" "s|${pattern}|${apl_mariadb_pkg_dir}|g" ${apl_mariadb_cnf_file}
 
 }
 
@@ -204,7 +176,7 @@ create_dir() {
 logfile() {
     local lc=$1
     [ "$lc" != "" ] || lc="common"
-    echo "$BASE_DIR/log/${lc}-$(date +%Y%m%d).log"
+    echo "$BASE_DIR/${lc}-$(date +%Y%m%d).log"
 }
 
 #Write message from stdin into log file and console at the same time
@@ -247,7 +219,6 @@ check_root_execution
 #stop the suspicious mariadbd pocess
 kill_service "${apl_mariadb_cnf_file}"
 
-mkdir -p ${BASE_DIR}/log
 echo "Script started at "`date "+%Y-%m-%d %H:%M:%S"` | writelog
 echo "cmd: $0 $cmd_params" | writelog
 echo "---" | writelog
@@ -268,7 +239,7 @@ check_mariadb_pkg
 
 [ ! -f ${apl_property_file} ] && exit_error "Can't locate the property file: ${apl_property_file}"
 
-all_steps=7
+all_steps=6
 curr_step=1
 
 echo "$curr_step/$all_steps. Create directories"  | writelog
@@ -305,10 +276,6 @@ echo "$curr_step/$all_steps. Check updated password"  | writelog
 echo "cmd: ${apl_mariadb_pkg_dir}/bin/mariadb --defaults-file=${apl_mariadb_cnf_file} -u ${db_user} -s -e 'select 1;'" | writelog
 ${apl_mariadb_pkg_dir}/bin/mariadb --defaults-file=${apl_mariadb_cnf_file} -u ${db_user} -s -e 'select 1;' | writelog
 [ ! $? -eq 0 ] && exit_error "Execution error."
-curr_step=$((curr_step+1))
-
-echo "$curr_step/$all_steps. Patch apl-blockchain.properties"  | writelog
-patch_blockchain_properties | writelog
 curr_step=$((curr_step+1))
 
 echo "Script done at "`date "+%Y-%m-%d %H:%M:%S"` | writelog
