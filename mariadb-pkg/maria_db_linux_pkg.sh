f0f40647d3285f59508c65ff815d08794bc7bd2e
Oleksiy Lukin <oleskiylukin@fritbridge.io>
2021-01-13 16:09:39 +0200

    Linux package now contains libncurses5 because newest Ubuntu in on ncurses6

--- mariadb-pkg/maria_db_linux_pkg.sh
+++ mariadb-pkg/maria_db_linux_pkg.sh
@@ -6,6 +6,13 @@ FNAME="$DIR_NAME.tar.gz"
 MARIADB_URL=https://downloads.mariadb.org/rest-api/mariadb/$MARIADB_VER/$FNAME
 target=target-linux
 
+NCURSES_URL=http://us.archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/
+NCURSES_NLIB=libncurses5_6.2-1_amd64.deb
+NCURSES_TLIB=libtinfo5_6.2-1_amd64.deb
+
+FILES=($FNAME $NCURSES_NLIB $NCURSES_TLIB)
+URLS=($MARIADB_URL $NCURSES_URL$NCURSES_NLIB $NCURSES_URL$NCURSES_TLIB)
+
 if ! wget -V >/dev/null
 then
  echo "Please install wget"
@@ -18,20 +25,85 @@ then
  exit 2
 fi
 
-DNLD_RES=0
-if [ -r $FNAME ]
+if ! patchelf --version >>/dev/null
 then
- echo "$FNAME already exists. Skiping download" 
-else
- wget $MARIADB_URL
- DNLD_RES=$?
+ echo "Please install patchelf"
+ exit 2
 fi
 
-if [ ! $DNLD_RES ]
-then
- echo "Download error!!!"
- exit 1
+DNLD_RES=0
+for index in ${!FILES[*]} ; do
+    file=${FILES[index]}
+    url=${URLS[index]}
+    if [ -r $file ]
+    then
+     echo "File: $file already exists. Skiping download" 
+    else
+     wget $url
+     res=$?
+     if [ $res ] ; then
+       echo "Download of file $file failed"
+       DNLD_RES=1
+     fi
+    fi
+done
+
+if [ $DNLD_RES ] ; then
+  echo "All files downloaded"
+else
+  echo "Download filed"
+  exit 1
 fi
+ 
+undeb() {
+ deb=$1
+ echo "extracting lib: $deb"
+ if [ -d debs ] ; then
+   echo "debs directory exists"
+ else
+   mkdir debs
+ fi
+ cd debs
+ ar x ../$deb
+ tar -xvaf data.tar.*
+ cd ../
+}
+
+copy_libs() {
+  echo "Copying libraries"
+  cp --preserve=links debs/lib/x86_64-linux-gnu/* $target/ApolloWallet/apollo-mariadb/bin
+}
+
+gen_pkg_json () {
+  cd $target/ApolloWallet
+  mkdir packaging
+  cd packaging
+  PF="pkg-apollo-mariadb.json"
+  echo "{" > $PF
+  echo "  \"name\": \"apollo-mariadb\"," >>$PF
+  echo "  \"description\": \"Apollo mariadb server\"," >>$PF
+  echo "  \"version\": \"$MARIADB_VER\"," >>$PF
+  echo "  \"dependencies\": [" >>$PF
+  echo "  ]" >>$PF
+  echo "}" >> $PF
+  cd ../../
+}
+
+delete_extra_from_dist(){
+  cd $target/ApolloWallet/apollo-mariadb
+  FILES_TO_DEL=""
+  DIRS_TO_DEL="include sql-bench man mysql-test"
+  for f in $FILES_TO_DEL; do
+    rm -f $f
+  done
+
+  for d in $DIRS_TO_DEL; do
+    rm -rf $d
+  done
+
+  cd ../../../
+}
+
 
 if [ -d $DIR_NAME ]
 then
@@ -44,23 +116,14 @@ if [ -d $target ]
 then
  rm -rf $target
 fi
+
 PWD=`pwd`
 mkdir $target
 cd $target
 mkdir ApolloWallet
 cd ApolloWallet
-mkdir packaging
-cd packaging
-PF="pkg-apollo-mariadb.json"
-echo "{" > $PF
-echo "  \"name\": \"apollo-mariadb\"," >>$PF
-echo "  \"description\": \"Apollo mariadb server\"," >>$PF
-echo "  \"version\": \"$MARIADB_VER\"," >>$PF
-echo "  \"dependencies\": [" >>$PF
-echo "  ]" >>$PF
-echo "}" >> $PF
-cd ../
 mv  ../../$DIR_NAME apollo-mariadb
+
 cd apollo-mariadb
 mkdir conf
 cp ../../../scripts/my-apl.cnf conf/my-apl.cnf.template
@@ -72,18 +135,14 @@ cp ../../../scripts/install-mariadb.sh ./
 [ ! $? -eq 0 ] && exit 1
 
 chmod +x install-mariadb.sh
+cd ../../../
 
-FILES_TO_DEL=""
-DIRS_TO_DEL="include sql-bench man mysql-test"
-for f in $FILES_TO_DEL; do
-  rm -f $f
-done
+undeb ${FILES[1]}
+undeb ${FILES[2]}
+copy_libs
 
-for d in $DIRS_TO_DEL; do
-  rm -rf $d
-done
-
-cd ../../
+delete_extra_from_dist
+gen_pkg_json
 
 zip --symlink -r apollo-mariadb-$MARIADB_VER-Linux-X86_64.zip ApolloWallet
 sha256sum  apollo-mariadb-$MARIADB_VER-Linux-X86_64.zip >  apollo-mariadb-$MARIADB_VER-Linux-X86_64.zip.sha256
